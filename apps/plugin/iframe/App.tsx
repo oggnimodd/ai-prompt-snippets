47199323f28c3568a1b0c051b530f18951cfea2a
dominggo1999 <arnolddominggo19@gmail.com>
2023-08-30 01:42:54 +0700

    add padding to iframe content

--- apps/plugin/iframe/App.tsx
+++ apps/plugin/iframe/App.tsx
@@ -2,7 +2,7 @@ import TextInput from "./TextInput";
 
 const App = () => {
   return (
-    <div className="w-full min-h-screen bg-[#202022] dark">
+    <div className="w-full min-h-screen bg-[#202022] dark p-2">
       <TextInput />
     </div>
   );
