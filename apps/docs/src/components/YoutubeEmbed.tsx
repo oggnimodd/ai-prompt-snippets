daed0294b0fb63003455d41e892b35aca05b3e89
oggnimodd <arnold.ds.dev@gmail.com>
2023-10-18 17:47:45 +0700

    fix react youtube error

--- apps/docs/src/components/YoutubeEmbed.tsx
+++ apps/docs/src/components/YoutubeEmbed.tsx
@@ -1,6 +1,13 @@
 import YouTube from "react-youtube";
 
 const YoutubeEmbed = () => {
+  const isDOM =
+    typeof window !== "undefined" &&
+    window.document &&
+    window.document.documentElement;
+
+  if (!isDOM) return <div />;
+
   return (
     <YouTube
       style={{
